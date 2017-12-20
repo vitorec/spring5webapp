29d6bfb13677d1c555841846e80a6284369b48c2
Vitor E. C <vitor.ec@gmail.com>
2017-12-20 13:42:47 -0200

    Creating Author controller and list of authors

---
+++ src/main/java/guru/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controller;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Vitor on 18/12/2017.
+ */
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+
+    }
+}
